16d6c17e34979978e6a72c9fe13a471223a0b1cc
Geumseong Han <geumseong.han@ue-germany.de>
2020-11-01 21:18:32 +0900

    Added Shop UI into the main scene (Not functioning yet). Added a font for UI. Currently in the middle of making Shop.cs script working.

---
+++ Gulayev_Han_Jox_GAW5/Assets/Scripts/Shop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    
+    public void PurchasePrimary() {
+        Debug.Log("Primary Weapon Upgrade Purchased");
+    }
+
+    public void PurchaseRocket() {
+        Debug.Log("Rocket Turret Purchased");
+    }
+
+    public void PurchaseLaser() {
+        Debug.Log("Laser Turret Purchased");
+    }
+
+    public void PurchaseIce() {
+        Debug.Log("Ice Turret Purchased");
+    }
+
+    public void PurchaseFuture() {
+        Debug.Log("Future Turret Purchased");
+    }
+
+}
