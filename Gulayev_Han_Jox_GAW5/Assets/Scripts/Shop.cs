065c86a49657cc6dcb8733ce7ae62993c41ad889
Geumseong Han <geumseong.han@ue-germany.de>
2020-11-02 04:20:27 +0900

    Item Selection in Shop Menu is available now.

--- Gulayev_Han_Jox_GAW5/Assets/Scripts/Shop.cs
+++ Gulayev_Han_Jox_GAW5/Assets/Scripts/Shop.cs
@@ -1,28 +1,120 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-    
-    public void PurchasePrimary() {
-        Debug.Log("Primary Weapon Upgrade Purchased");
+    public GameStateManager gameStateManager;
+    string selected;
+    public GameObject primaryButton;
+    public GameObject rocketButton;
+    public GameObject laserButton;
+    public GameObject iceButton;
+    public GameObject futureButton;
+    public GameObject healthButton;
+    public List<Sprite> primarySprite;
+    public List<Sprite> rocketSprite;
+    public List<Sprite> laserSprite;
+    public List<Sprite> iceSprite;
+    public List<Sprite> futureSprite;
+    public List<Sprite> healthSprite;
+
+
+    public void Stater() {
+        gameStateManager = GameObject.Find("GameStateManager").GetComponent<GameStateManager>();
     }
 
-    public void PurchaseRocket() {
-        Debug.Log("Rocket Turret Purchased");
+    public void Update()
+    {
+       
     }
 
-    public void PurchaseLaser() {
-        Debug.Log("Laser Turret Purchased");
+    public void PressOnPrimary() {
+        selected = "primary";
+        primaryButton.GetComponent<Image>().sprite = primarySprite[1];
+        rocketButton.GetComponent<Image>().sprite = rocketSprite[0];
+        laserButton.GetComponent<Image>().sprite = laserSprite[0];
+        iceButton.GetComponent<Image>().sprite = iceSprite[0];
+        futureButton.GetComponent<Image>().sprite = futureSprite[0];
+        healthButton.GetComponent<Image>().sprite = healthSprite[0];
     }
 
-    public void PurchaseIce() {
-        Debug.Log("Ice Turret Purchased");
+    public void PressOnRocket() {
+        selected = "rocket";
+        primaryButton.GetComponent<Image>().sprite = primarySprite[0];
+        rocketButton.GetComponent<Image>().sprite = rocketSprite[1];
+        laserButton.GetComponent<Image>().sprite = laserSprite[0];
+        iceButton.GetComponent<Image>().sprite = iceSprite[0];
+        futureButton.GetComponent<Image>().sprite = futureSprite[0];
+        healthButton.GetComponent<Image>().sprite = healthSprite[0];
     }
 
-    public void PurchaseFuture() {
-        Debug.Log("Future Turret Purchased");
+    public void PressOnLaser() {
+        selected = "laser";
+        primaryButton.GetComponent<Image>().sprite = primarySprite[0];
+        rocketButton.GetComponent<Image>().sprite = rocketSprite[0];
+        laserButton.GetComponent<Image>().sprite = laserSprite[1];
+        iceButton.GetComponent<Image>().sprite = iceSprite[0];
+        futureButton.GetComponent<Image>().sprite = futureSprite[0];
+        healthButton.GetComponent<Image>().sprite = healthSprite[0];
     }
 
+    public void PressOnIce() {
+        selected = "ice";
+        primaryButton.GetComponent<Image>().sprite = primarySprite[0];
+        rocketButton.GetComponent<Image>().sprite = rocketSprite[0];
+        laserButton.GetComponent<Image>().sprite = laserSprite[0];
+        iceButton.GetComponent<Image>().sprite = iceSprite[1];
+        futureButton.GetComponent<Image>().sprite = futureSprite[0];
+        healthButton.GetComponent<Image>().sprite = healthSprite[0];
+    }
+
+    public void PressOnFuture() {
+        selected = "future";
+        primaryButton.GetComponent<Image>().sprite = primarySprite[0];
+        rocketButton.GetComponent<Image>().sprite = rocketSprite[0];
+        laserButton.GetComponent<Image>().sprite = laserSprite[0];
+        iceButton.GetComponent<Image>().sprite = iceSprite[0];
+        futureButton.GetComponent<Image>().sprite = futureSprite[1];
+        healthButton.GetComponent<Image>().sprite = healthSprite[0];
+    }
+
+    public void PressOnHealth() {
+        selected = "health";
+        primaryButton.GetComponent<Image>().sprite = primarySprite[0];
+        rocketButton.GetComponent<Image>().sprite = rocketSprite[0];
+        laserButton.GetComponent<Image>().sprite = laserSprite[0];
+        iceButton.GetComponent<Image>().sprite = iceSprite[0];
+        futureButton.GetComponent<Image>().sprite = futureSprite[0];
+        healthButton.GetComponent<Image>().sprite = healthSprite[1];
+    }
+
+    public void Buy() {
+        switch(selected) {
+            case "primary":
+                gameStateManager.DecreaseMoney(100);
+                break;
+            case "rocket":
+                gameStateManager.DecreaseMoney(100);
+                break;
+            case "laser":
+                gameStateManager.DecreaseMoney(300);
+                break;
+            case "ice":
+                gameStateManager.DecreaseMoney(300);
+                break;
+            case "future":
+                gameStateManager.DecreaseMoney(500);
+                break;
+            case "health":
+                gameStateManager.DecreaseMoney(200);
+                break;
+        }
+    }
+
+    public void Sell() {
+
+    }
 }
